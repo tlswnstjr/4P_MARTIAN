5a2dbf69a17ef481a9cdb8c6e3469d5d316362c0
tlswnstjr <45341813+tlswnstjr@users.noreply.github.com>
2020-06-03 13:20:52 +0900

    2236

---
+++ MARTIAN/Assets/SJS/J_Scripts/J_CarMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_CarMove : MonoBehaviour
+{
+    public float MotorF;
+
+    public WheelCollider wheelBR;
+    public WheelCollider wheelFR;
+
+
+    public WheelCollider wheelBL;
+    public WheelCollider wheelFL;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
