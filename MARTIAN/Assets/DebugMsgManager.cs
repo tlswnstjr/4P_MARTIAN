9b18ce5770aed1bba73ab4220923ee81b5558c70
lsm9710 <54009854+lsm9710@users.noreply.github.com>
2020-06-08 18:31:46 +0900

    빌드할때 알파레이어 작동안하는 버그 수정

---
+++ MARTIAN/Assets/DebugMsgManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugMsgManager : MonoBehaviour
+{
+    public static DebugMsgManager Instance;
+    public Text debugText;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+
+       
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(transform.parent.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
