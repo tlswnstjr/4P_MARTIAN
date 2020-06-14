bb954f9a7fbf7479fc1cb7eee05ad6d309b060ed
lsm9710 <54009854+lsm9710@users.noreply.github.com>
2020-06-15 08:46:15 +0900

    사운드 빠진부분 작업

--- MARTIAN/Assets/SM_Folder/Scripts/Door/Door3_Opening.cs
+++ MARTIAN/Assets/SM_Folder/Scripts/Door/Door3_Opening.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class Door3_Opening : MonoBehaviour
 {
     Animator anim;
+    AudioSource audioPlayer;
+    public AudioClip doorSound;
 
     public BoxCollider bottom_A, bottom_B;
 
     private void Start()
     {
+        audioPlayer = GetComponent<AudioSource>();
         anim = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         anim.SetBool("character_nearby", true);
+        if (!audioPlayer.isPlaying)
+        {
+            audioPlayer.clip = doorSound;
+            audioPlayer.Play();
+
+        }
         bottom_A.isTrigger = true;
         bottom_B.isTrigger = true;
     }
